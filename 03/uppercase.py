38739489358719e81a798b4427999f6712559a25
Goliasss <79666798+Goliasss@users.noreply.github.com>
2021-03-29 21:18:14 +0200

    uprava

--- 03/uppercase.py
+++ 03/uppercase.py
@@ -5,6 +5,9 @@ import os
 import re
 os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "2") # Report only TF errors by default
 
+# a507688b-17c7-11e8-9de3-00505601122b
+# bee39584-17d2-11e8-9de3-00505601122b
+
 import numpy as np
 import tensorflow as tf
 
@@ -15,10 +18,13 @@ from uppercase_data import UppercaseData
 parser = argparse.ArgumentParser()
 parser.add_argument("--alphabet_size", default=50, type=int, help="If nonzero, limit alphabet to this many most frequent chars.")
 parser.add_argument("--batch_size", default=50, type=int, help="Batch size.")
-parser.add_argument("--epochs", default=1, type=int, help="Number of epochs.")
+parser.add_argument("--epochs", default=5, type=int, help="Number of epochs.")
 parser.add_argument("--seed", default=42, type=int, help="Random seed.")
 parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
 parser.add_argument("--window", default=6, type=int, help="Window size to use.")
+parser.add_argument("--dropout", default=0.5, type=float, help="Dropout regularization.")
+parser.add_argument("--label_smoothing", default=0, type=float, help="Label smoothing.")
+parser.add_argument("--hidden_layers", default=[200, 200], nargs="*", type=int, help="Hidden layer sizes.")
 
 def main(args):
     # Fix random seeds and threads
@@ -37,59 +43,50 @@ def main(args):
     # Load data
     uppercase_data = UppercaseData(args.window, args.alphabet_size)
 
-    train = False       # True - train a model
+    train = False      # True - train a model
     testing = False     # True - cropping train data for debugging the whole thing
 
     if testing:
         uppercase_data.train.data["windows"] = uppercase_data.train.data["windows"][0:1000]
         uppercase_data.train.data["labels"] = uppercase_data.train.data["labels"][0:1000]
 
-    # TODO: Implement a suitable model, optionally including regularization, select
-    # good hyperparameters and train the model.
-    #
-    # The inputs are _windows_ of fixed size (`args.window` characters on left,
-    # the character in question, and `args.window` characters on right), where
-    # each character is represented by a `tf.int32` index. To suitably represent
-    # the characters, you can:
-    # - Convert the character indices into _one-hot encoding_. There is no
-    #   explicit Keras layer, but you can
-    #   - use a Lambda layer which can encompass any function:
-    #       tf.keras.Sequential([
-    #         tf.keras.layers.InputLayer(input_shape=[2 * args.window + 1], dtype=tf.int32),
-    #         tf.keras.layers.Lambda(lambda x: tf.one_hot(x, len(uppercase_data.train.alphabet))),
-    #   - or use Functional API and then any TF function can be used
-    #     as a Keras layer:
-    #       inputs = tf.keras.layers.Input(shape=[2 * args.window + 1], dtype=tf.int32)
-    #       encoded = tf.one_hot(inputs, len(uppercase_data.train.alphabet))
-    #   You can then flatten the one-hot encoded windows and follow with a dense layer.
-    # - Alternatively, you can use `tf.keras.layers.Embedding` (which is an efficient
-    #   implementation of one-hot encoding followed by a Dense layer) and flatten afterwards.
+    if args.label_smoothing == 0:
+        loss = tf.losses.SparseCategoricalCrossentropy()
+        metrics = tf.metrics.SparseCategoricalAccuracy(name="accuracy")
+    else:
+        loss = tf.losses.CategoricalCrossentropy(label_smoothing=args.label_smoothing)
+        metrics = tf.metrics.CategoricalAccuracy(name="accuracy")
+        uppercase_data.train.data["labels"] = tf.keras.utils.to_categorical(uppercase_data.train.data["labels"])
+        uppercase_data.dev.data["labels"] = tf.keras.utils.to_categorical(uppercase_data.dev.data["labels"])
+        uppercase_data.test.data["labels"] = tf.keras.utils.to_categorical(uppercase_data.test.data["labels"])
+
+
     def create_model():
-        model = tf.keras.Sequential([
-            tf.keras.layers.InputLayer(input_shape=[2 * args.window + 1], dtype=tf.int32),
-            tf.keras.layers.Lambda(lambda x: tf.one_hot(x, len(uppercase_data.train.alphabet))),
-            tf.keras.layers.Flatten(),
-            tf.keras.layers.Dense(units=256, activation=tf.nn.relu),
-            tf.keras.layers.Dense(units=128, activation=tf.nn.relu),
-            tf.keras.layers.Dense(units=2, activation=tf.nn.softmax),
-        ])
+        model = tf.keras.Sequential()
+        model.add(tf.keras.layers.InputLayer(input_shape=[2 * args.window + 1], dtype=tf.int32))
+        model.add(tf.keras.layers.Lambda(lambda x: tf.one_hot(x, len(uppercase_data.train.alphabet))))
+        model.add(tf.keras.layers.Flatten())
+        if args.dropout > 0:
+            model.add(tf.keras.layers.Dropout(args.dropout))
+        for hidden_layer in args.hidden_layers:
+            model.add(tf.keras.layers.Dense(hidden_layer, activation=tf.nn.relu))
+            if args.dropout > 0:
+                model.add(tf.keras.layers.Dropout(args.dropout))
+        model.add(tf.keras.layers.Dense(2, activation=tf.nn.softmax))
+
         return model
+
     def compile(model):
         model.compile(
             optimizer=tf.optimizers.Adam(),
-            loss=tf.losses.SparseCategoricalCrossentropy(),
-            metrics=[tf.metrics.SparseCategoricalAccuracy("accuracy")],
+            loss=loss,
+            metrics=[metrics],
         )
 
     if train:
         model = create_model()
         model.summary()
-        model.compile(
-            optimizer=tf.optimizers.Adam(),
-            loss=tf.losses.SparseCategoricalCrossentropy(),
-            metrics=[tf.metrics.SparseCategoricalAccuracy("accuracy")],
-        )
-
+        compile(model)
         model.fit(
             uppercase_data.train.data["windows"], uppercase_data.train.data["labels"],
             batch_size=args.batch_size, epochs=args.epochs,
